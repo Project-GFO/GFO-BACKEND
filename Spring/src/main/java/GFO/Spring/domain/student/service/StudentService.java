aebe7a97081fe98ca85c0672b5814be6ff124597
김태오 <taeo0328@gmail.com>
2022-11-02 19:56:45 +0900

    ãService ìbean ãregistration

--- Spring/src/main/java/GFO/Spring/domain/student/service/StudentService.java
+++ Spring/src/main/java/GFO/Spring/domain/student/service/StudentService.java
@@ -3,8 +3,10 @@ package GFO.Spring.domain.student.service;
 import GFO.Spring.domain.student.Student;
 import GFO.Spring.domain.student.repository.StudentRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
+@Service
 public class StudentService {
     private final StudentRepo studentRepo;
 
