3a824a4cf0065ae2510a1c5e6e909c84eafd7162
김태오 <taeo0328@gmail.com>
2022-11-04 16:14:03 +0900

    DTO에 Validation 및 email exsistBy작성

--- Spring/src/main/java/GFO/Spring/domain/User/presentation/dto/request/SignupRequest.java
+++ Spring/src/main/java/GFO/Spring/domain/User/presentation/dto/request/SignupRequest.java
@@ -4,13 +4,23 @@ import GFO.Spring.domain.User.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
+import javax.validation.constraints.*;
+
 @Getter
 @NoArgsConstructor
 public class SignupRequest {
+
+    @Email
+    @NotEmpty(message = "이메일은 필수 입력값입니다")
     private String email;
+    @NotEmpty(message = "이름은 필수 입력값입니다")
     private String name;
+    @NotEmpty(message = "비밀번호는 필수 입력값입니다")
     private String password;
+    @NotEmpty(message = "필수 입력란입니다")
     private String duty;
+    @NotEmpty(message = "학번은 필수 입력값입니다")
     private int classNum;
 
     public User toEntity() {
