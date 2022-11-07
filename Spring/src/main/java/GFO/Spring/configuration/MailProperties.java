1bc4a554004fa7d182ebb81a2f452a54895d9d54
채종인 <105656208+Chaejongin12@users.noreply.github.com>
2022-11-08 08:26:26 +0900

    Properties
    Properties

--- Spring/src/main/java/GFO/Spring/configuration/MailProperties.java
+++ Spring/src/main/java/GFO/Spring/configuration/MailProperties.java
@@ -11,6 +11,7 @@ import org.springframework.boot.context.properties.ConstructorBinding;
 @ConstructorBinding
 @ConfigurationProperties(prefix = "spring.mail")
 public class MailProperties {
+    //커밋 실수로 주석한번 답니다.
     private String host;
     private Integer port;
     private String username;
--- Spring/src/main/java/GFO/Spring/controller/ApiResponses.java
+++ Spring/src/main/java/GFO/Spring/controller/ApiResponses.java
@@ -1,1 +1,2 @@
 package GFO.Spring.controller;
+//커밋 실수로 주석한번 답니다
