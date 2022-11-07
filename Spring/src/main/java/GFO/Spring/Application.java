782c87358d39e267f7b76dd498e6136005620b66
채종인 <105656208+Chaejongin12@users.noreply.github.com>
2022-11-08 08:19:55 +0900

    Application
    Application

--- Spring/src/main/java/GFO/Spring/Application.java
+++ Spring/src/main/java/GFO/Spring/Application.java
@@ -2,8 +2,12 @@ package GFO.Spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan
 public class Application {
 
 	public static void main(String[] args) {
