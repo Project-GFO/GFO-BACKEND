ab4b0696fb3e8c6d8b45960020b5dfc2a78e1fe9
김태오 <taeo0328@gmail.com>
2022-11-14 20:07:09 +0900

    Bean이 Interface를주입받도록 수정

--- Spring/src/main/java/GFO/Spring/global/security/SecurityConfig.java
+++ Spring/src/main/java/GFO/Spring/global/security/SecurityConfig.java
@@ -1,14 +1,16 @@
 package GFO.Spring.global.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig  {
-
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
