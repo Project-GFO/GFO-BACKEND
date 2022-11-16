96e4de9379aa27de55527d8a7404eabdcbbd63ae
김태오 <taeo0328@gmail.com>
2022-11-16 19:40:58 +0900

    RefreshToken의 DTO 작성

---
+++ Spring/src/main/java/GFO/Spring/global/security/jwt/dto/TokenResponse.java
@@ -0,0 +1,11 @@
+package GFO.Spring.global.security.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+    private final String accessToken;
+    private final String refreshToken;
+}
