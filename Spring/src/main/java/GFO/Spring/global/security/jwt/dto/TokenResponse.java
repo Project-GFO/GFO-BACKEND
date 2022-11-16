0087e21159d84351518ce86e6270c32340cb4042
김태오 <taeo0328@gmail.com>
2022-11-16 22:31:33 +0900

    AllArgs -> RequiredArgs

--- Spring/src/main/java/GFO/Spring/global/security/jwt/dto/TokenResponse.java
+++ Spring/src/main/java/GFO/Spring/global/security/jwt/dto/TokenResponse.java
@@ -1,10 +1,10 @@
 package GFO.Spring.global.security.jwt.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class TokenResponse {
     private final String accessToken;
     private final String refreshToken;
