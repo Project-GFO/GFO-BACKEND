9580a586f997491ea4ddd4cff5f6af05b787e0c5
김태오 <taeo0328@gmail.com>
2022-11-28 21:26:38 +0900

    db에 유저가 있다면 user를 반환하고 없다면 exception을 터뜨리는 UserUtil 작성

--- Spring/src/main/java/GFO/Spring/global/util/UserUtil.java
+++ Spring/src/main/java/GFO/Spring/global/util/UserUtil.java
@@ -2,7 +2,7 @@ package GFO.Spring.global.util;
 
 import GFO.Spring.domain.user.entity.User;
 import GFO.Spring.domain.user.repository.UserRepository;
-import GFO.Spring.global.exception.exceptioncollection.UserNotFoundException;
+import GFO.Spring.domain.user.exception.exceptioncollection.UserNotFoundException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 public class UserUtil {
     private UserRepository userRepository;
 
-    public User CurrentUser() {
+    public User currentUser() {
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
         return userRepository.findUserByEmail(email).orElseThrow(()->new UserNotFoundException("유저가 없습니다"));
     }
