a478484603c6f22f1843fb270eef45b7a25e0e9f
김태오 <taeo0328@gmail.com>
2022-12-06 08:42:45 +0900

    wrapper 클래스로 타입 변경

--- Spring/src/main/java/GFO/Spring/domain/user/entity/BlackList.java
+++ Spring/src/main/java/GFO/Spring/domain/user/entity/BlackList.java
@@ -3,6 +3,7 @@ package GFO.Spring.domain.user.entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
@@ -10,12 +11,14 @@ import org.springframework.data.redis.core.TimeToLive;
 import java.time.ZonedDateTime;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
-@RedisHash()
+@RedisHash("blackList")
 public class BlackList {
     @Id
     private String accessToken;
     private String email;
     @TimeToLive
-    private ZonedDateTime timeToLive;
+    private Long timeToLive;
 }
