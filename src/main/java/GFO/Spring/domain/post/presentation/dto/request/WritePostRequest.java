34d3af28543cb3fdce7e669a8bbaf4628f66be20
KimTaeO <taeo0328@gmail.com>
2023-01-20 14:21:02 +0900

    validation 어노테이션 추가

--- src/main/java/GFO/Spring/domain/post/presentation/dto/request/WritePostRequest.java
+++ src/main/java/GFO/Spring/domain/post/presentation/dto/request/WritePostRequest.java
@@ -5,14 +5,16 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Getter
 @Builder
 @RequiredArgsConstructor
 public class WritePostRequest {
+    @NotBlank(message = "제목은 필수 입력값입니다")
     private final String title;
+    @NotBlank(message = "내용은 필수 입력값입니다")
     private final String content;
-
     private final List<String> contentFileURLs;
 }
