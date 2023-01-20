4a6693db1c47d34f4ddf82631c872af128effdc1
KimTaeO <taeo0328@gmail.com>
2023-01-20 14:21:57 +0900

    왜 붙어있는지 모르겠는 repository어노테이션 삭제

--- src/main/java/GFO/Spring/domain/post/repository/PostRepository.java
+++ src/main/java/GFO/Spring/domain/post/repository/PostRepository.java
@@ -2,9 +2,6 @@ package GFO.Spring.domain.post.repository;
 
 import GFO.Spring.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-
 }
