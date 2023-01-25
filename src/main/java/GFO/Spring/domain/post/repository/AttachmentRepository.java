2f562516170fa7a20f62d90454ed64bd05361f4d
KimTaeO <taeo0328@gmail.com>
2023-01-25 22:07:48 +0900

    첨부파일 리포지토리 CrudRepository를 상속받는 것으로 변경

--- src/main/java/GFO/Spring/domain/post/repository/AttachmentRepository.java
+++ src/main/java/GFO/Spring/domain/post/repository/AttachmentRepository.java
@@ -2,6 +2,7 @@ package GFO.Spring.domain.post.repository;
 
 import GFO.Spring.domain.post.entity.Attachment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
+public interface AttachmentRepository extends CrudRepository<Attachment, Long> {
 }
