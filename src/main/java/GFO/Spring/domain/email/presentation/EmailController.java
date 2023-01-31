8ac067af73a5d7d975929388aab04f6708a55100
KimTaeO <taeo0328@gmail.com>
2023-01-31 21:56:36 +0900

    이메일 인증 컨트롤러 작성

---
+++ src/main/java/GFO/Spring/domain/email/presentation/EmailController.java
@@ -0,0 +1,30 @@
+package GFO.Spring.domain.email.presentation;
+
+import GFO.Spring.domain.email.entity.EmailAuth;
+import GFO.Spring.domain.email.presentation.dto.request.EmailSendDto;
+import GFO.Spring.domain.email.service.EmailAuthService;
+import GFO.Spring.domain.email.service.EmailSendService;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Email;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/email")
+public class EmailController {
+    private final EmailSendService emailSendService;
+    private final EmailAuthService emailAuthService;
+    @PostMapping
+    public ResponseEntity<Void> authEmail(@RequestBody @Valid EmailSendDto emailSendDto) {
+        emailSendService.execute(emailSendDto);
+        return ResponseEntity.ok().build();
+    }
+    @RequestMapping(method = RequestMethod.HEAD)
+    public ResponseEntity<Void> mailVerify(@Email @RequestParam String email, @RequestParam String authKey){
+        emailAuthService.execute(email,authKey);
+        return ResponseEntity.ok().build();
+    }
+}
