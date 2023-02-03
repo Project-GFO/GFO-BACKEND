b7ce42f768aa4b60b050a186fcc5ba17e27aa7f9
KimTaeO <taeo0328@gmail.com>
2023-02-03 13:43:29 +0900

    게시물 수정 controller 작성

--- src/main/java/GFO/Spring/domain/post/presentation/PostController.java
+++ src/main/java/GFO/Spring/domain/post/presentation/PostController.java
@@ -1,14 +1,13 @@
 package GFO.Spring.domain.post.presentation;
 
 import GFO.Spring.domain.post.presentation.dto.request.CreatePostRequest;
+import GFO.Spring.domain.post.presentation.dto.request.ModifyPostRequest;
 import GFO.Spring.domain.post.service.CreatePostService;
+import GFO.Spring.domain.post.service.ModifyPostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -18,10 +17,17 @@ import javax.validation.Valid;
 public class PostController {
 
     private final CreatePostService createPostService;
+    private final ModifyPostService modifyPostService;
 
     @PostMapping
     public ResponseEntity<Void> createPost(@RequestBody @Valid CreatePostRequest createPostRequest) {
         createPostService.execute(createPostRequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> modifyPost(@PathVariable Long id,  @RequestBody @Valid ModifyPostRequest modifyPostRequest) {
+        modifyPostService.execute(id, modifyPostRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
