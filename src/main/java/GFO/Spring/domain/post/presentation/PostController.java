7673c77f92119e2e827be4ae55f7637b0f1a5f62
KimTaeO <taeo0328@gmail.com>
2023-02-06 16:03:04 +0900

    controller 로직 작성

--- src/main/java/GFO/Spring/domain/post/presentation/PostController.java
+++ src/main/java/GFO/Spring/domain/post/presentation/PostController.java
@@ -4,6 +4,7 @@ import GFO.Spring.domain.post.presentation.dto.request.CreatePostRequest;
 import GFO.Spring.domain.post.presentation.dto.request.ModifyPostRequest;
 import GFO.Spring.domain.post.service.CreatePostService;
 import GFO.Spring.domain.post.service.ModifyPostService;
+import GFO.Spring.domain.post.service.RemovePostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ public class PostController {
 
     private final CreatePostService createPostService;
     private final ModifyPostService modifyPostService;
+    private final RemovePostService removePostService;
 
     @PostMapping
     public ResponseEntity<Void> createPost(@RequestBody @Valid CreatePostRequest createPostRequest) {
@@ -29,5 +31,11 @@ public class PostController {
     public ResponseEntity<Void> modifyPost(@PathVariable Long id,  @RequestBody @Valid ModifyPostRequest modifyPostRequest) {
         modifyPostService.execute(id, modifyPostRequest);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> removePost(@PathVariable Long id) {
+        removePostService.execute(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
