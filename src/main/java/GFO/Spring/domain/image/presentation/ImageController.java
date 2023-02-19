7f8af1908ab78f957cdfb3c74f077663b745fbfc
KimTaeO <taeo0328@gmail.com>
2023-02-20 02:55:36 +0900

    :sparkles: multiPartFile 받는 곳 RequestPart로 변경

--- src/main/java/GFO/Spring/domain/image/presentation/ImageController.java
+++ src/main/java/GFO/Spring/domain/image/presentation/ImageController.java
@@ -4,10 +4,7 @@ import GFO.Spring.domain.image.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -20,7 +17,7 @@ public class ImageController {
     private final ImageService imageService;
 
     @PostMapping
-    public ResponseEntity<Void> createImage(@RequestParam Long postId, @RequestParam("file") List<MultipartFile> images) throws Exception {
+    public ResponseEntity<Void> createImage(@RequestParam("id") Long postId, @RequestPart("images") List<MultipartFile> images) throws Exception {
         imageService.execute(postId, images);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
