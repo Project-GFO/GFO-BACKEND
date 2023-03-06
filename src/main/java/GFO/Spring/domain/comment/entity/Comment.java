328eef4c6f8f999c027c397340ee8bf1d93db413
KimTaeO <taeo0328@gmail.com>
2023-03-06 16:12:45 +0900

    :sparkles: 댓글 엔티티 작성

---
+++ src/main/java/GFO/Spring/domain/comment/entity/Comment.java
@@ -0,0 +1,29 @@
+package GFO.Spring.domain.comment.entity;
+
+import GFO.Spring.domain.post.entity.Post;
+import GFO.Spring.domain.user.entity.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String comment;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
