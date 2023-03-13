36993f39317ed96615b999bb92ce80bf1dc02017
KimTaeO <taeo0328@gmail.com>
2023-03-13 17:18:17 +0900

    :sparkles: 댓글 수정 controller 추가

--- src/main/java/GFO/Spring/domain/comment/presentation/CommentController.java
+++ src/main/java/GFO/Spring/domain/comment/presentation/CommentController.java
@@ -1,7 +1,9 @@
 package GFO.Spring.domain.comment.presentation;
 
 import GFO.Spring.domain.comment.presentation.dto.request.CreateCommentReqDto;
+import GFO.Spring.domain.comment.presentation.dto.request.ModifyCommentReqDto;
 import GFO.Spring.domain.comment.service.CreateCommentService;
+import GFO.Spring.domain.comment.service.ModifyCommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +16,17 @@ import javax.validation.Valid;
 @RequestMapping("/comment")
 public class CommentController {
     private final CreateCommentService createCommentService;
+    private final ModifyCommentService modifyCommentService;
 
     @PostMapping("/{id}")
     public ResponseEntity<Void> createComment(@PathVariable Long id, @RequestBody @Valid CreateCommentReqDto createCommentReqDto) {
         createCommentService.execute(id, createCommentReqDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Void> modifyComment(@PathVariable Long id, @RequestBody @Valid ModifyCommentReqDto modifyCommentReqDto) {
+        modifyCommentService.execute(id, modifyCommentReqDto);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
