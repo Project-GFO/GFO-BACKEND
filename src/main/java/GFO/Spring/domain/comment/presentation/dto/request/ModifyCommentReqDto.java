8580315c6738e909d4db9dab9dc98b18cf6ac254
KimTaeO <taeo0328@gmail.com>
2023-03-13 17:14:13 +0900

    :sparkles: 댓글 수정 requestDto 작성

--- src/main/java/GFO/Spring/domain/comment/presentation/dto/request/ModifyCommentReqDto.java
+++ src/main/java/GFO/Spring/domain/comment/presentation/dto/request/ModifyCommentReqDto.java
@@ -1,2 +1,15 @@
-package GFO.Spring.domain.comment.presentation.dto.request;public class ModifyCommentReqDto {
+package GFO.Spring.domain.comment.presentation.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ModifyCommentReqDto {
+    @NotBlank(message = "수정된 댓글은 공백일 수 없습니다")
+    private String comment;
 }
